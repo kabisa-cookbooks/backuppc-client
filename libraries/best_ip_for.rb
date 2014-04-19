282a79b5152bcf6c6671ca83448e12c1d7306d88
Jean Mertz <jean@mertz.fm>
2014-04-20 00:47:54 +0200

    update BestIpFor module documentation

--- libraries/best_ip_for.rb
+++ libraries/best_ip_for.rb
@@ -1,4 +1,12 @@
-# Returns best ip based for other node on cloud/no-cloud if/else conditions.
+#
+# The best IP address for the given node, in the context of the current node.
+# Useful for choosing a local IP address over a public one to limit bandwidth on
+# cloud providers.
+#
+# See: https://github.com/sethvargo/chef-sugar#ip
+#
+# @param [Chef::Node] other
+#   the node to calculate the best IP address for
 #
 module BestIpFor
   def best_ip_for(other)
